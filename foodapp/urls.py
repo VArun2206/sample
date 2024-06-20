6efb560c4746f853389a27206ebf953ec41d773b
VArun <130547051+VArun2206@users.noreply.github.com>
2024-06-20 15:42:02 +0530

    Upate

---
+++ foodapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home),
+    path('menu/',views.menu),
+    path('about/',views.about),
+    path('book/',views.book),
+]
